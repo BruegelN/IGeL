0543ea358d45711653f6f63c66121d6856d70804
Nico Brügel <nico.bruegel@htwg-konstanz.de>
2019-10-18 14:34:58 +0200

    simplify center

--- src/center.cpp
+++ src/center.cpp
@@ -8,11 +8,7 @@ int main(int argc, char* argv[])
 
     igl::read_triangle_mesh("/home/nico/git/libigl/tutorial/data/cow.off", V,F);
 
-    Eigen::Vector3d means = V.colwise().mean();
-    V.col(0).array() -= means(0);
-    V.col(1).array() -= means(1);
-    V.col(2).array() -= means(2);
-
+    V = V.rowwise() - V.colwise().mean();
     igl::write_triangle_mesh("center.obj", V, F);
     return 0;
 }
