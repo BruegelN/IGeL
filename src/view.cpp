175c0826856b8da7c7321cca54f5883a97102919
BruegelN <BruegelN@users.noreply.github.com>
2019-12-04 01:53:37 +0100

    Added CLI to view. Added button to take screen shot therefor use igl::png

--- src/view.cpp
+++ src/view.cpp
@@ -3,14 +3,24 @@
 #include <igl/opengl/glfw/imgui/ImGuiMenu.h>
 #include <igl/opengl/glfw/imgui/ImGuiHelpers.h>
 #include <imgui/imgui.h>
+#include <igl/png/writePNG.h>
+#include "CLI11.hpp"
 
 
 int main(int argc, char *argv[])
 {
+  CLI::App app{"Use the libigl viewer to view a triangle mesh."};
+
   Eigen::MatrixXd V;
   Eigen::MatrixXi F;
+  std::string in_file, screenshot_file = "screenshot.png";
 
-  igl::read_triangle_mesh("/home/nico/git/libigl/tutorial/data/cow.off", V,F);
+  app.add_option("-i,--input", in_file, "The triangle mesh you want to view.")->required();
+  app.add_option("-s,--screenshot", in_file, "Screenshot file. Default is 'screenshot.png");
+
+  CLI11_PARSE(app, argc, argv);
+
+  igl::read_triangle_mesh(in_file, V,F);
 
   // Init the viewer
   igl::opengl::glfw::Viewer viewer;
@@ -24,6 +34,22 @@ int main(int argc, char *argv[])
   {
     // Draw parent menu content
     menu.draw_viewer_menu();
+    ImGui::Separator();
+    if (ImGui::Button("Take screenshot (2560×1600)", ImVec2(-1, 0)))
+    {
+      // Allocate temporary buffers
+      Eigen::Matrix<unsigned char,Eigen::Dynamic,Eigen::Dynamic> R(2560,1600);
+      Eigen::Matrix<unsigned char,Eigen::Dynamic,Eigen::Dynamic> G(2560,1600);
+      Eigen::Matrix<unsigned char,Eigen::Dynamic,Eigen::Dynamic> B(2560,1600);
+      Eigen::Matrix<unsigned char,Eigen::Dynamic,Eigen::Dynamic> A(2560,1600);
+
+      // Draw the scene in the buffers
+      viewer.core().draw_buffer(
+        viewer.data(),false,R,G,B,A);
+
+      // Save it to a PNG
+      igl::png::writePNG(R,G,B,A,screenshot_file);
+    }
   };
 
   // Plot the mesh
