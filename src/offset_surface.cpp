621c7c136bb9d743ea086dae08aca846db9da793
BruegelN <BruegelN@users.noreply.github.com>
2019-12-04 01:25:55 +0100

    Always compile offset surface and integrated cli.

--- src/offset_surface.cpp
+++ src/offset_surface.cpp
@@ -2,23 +2,33 @@
 #include <igl/write_triangle_mesh.h>
 #include <igl/signed_distance.h>
 #include <igl/copyleft/offset_surface.h>
+#include "CLI11.hpp"
 
 int main(int argc, char *argv[])
 {
+    CLI::App app{"Calculate the offset surface by running marching cube on a singed distance field of the given triangle mesh.\nMight close some holes!"};
+
     Eigen::MatrixXd V,SV;
     Eigen::MatrixXi F,SF;
     Eigen::MatrixXd GV;
     Eigen::VectorXd S;
     Eigen::RowVector3i side;
 
-    // TODO read params
+    std::string in_file, out_file = "out.obj";
     double isolevel = 0.01;
     int s = 100;
     igl::SignedDistanceType signed_distance_type = igl::SignedDistanceType::SIGNED_DISTANCE_TYPE_DEFAULT;
 
-    igl::read_triangle_mesh("/home/nico/git/libigl/tutorial/data/cow.off", V,F);
+    app.add_option("-i,--input", in_file, "The file you want to split into muliple files.")->required();
+    app.add_option("-o,--output", out_file, "The (expanded/shrinked) output file. Default is 'out.obj'");
+    app.add_option("-l,--isolevel", isolevel, "Iso level to extract (signed distance: negative inside)");
+    app.add_option("-s,--grid-resolution", s, "Number of grid cells along longest side (controls resolution).");
+
+    CLI11_PARSE(app, argc, argv);
+
+    igl::read_triangle_mesh(in_file, V,F);
     igl::copyleft::offset_surface(V, F, isolevel,s , signed_distance_type, SV, SF, GV, side, S);
-    igl::write_triangle_mesh("out.obj", SV,SF);
+    igl::write_triangle_mesh(out_file, SV,SF);
 
     return 0;
 
