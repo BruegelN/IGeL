477ced33589fd7c4e9726691bd25cd1c1dac7210
BruegelN <BruegelN@users.noreply.github.com>
2019-12-04 01:51:57 +0100

    added CLI for center

--- src/center.cpp
+++ src/center.cpp
@@ -1,14 +1,23 @@
 #include <igl/read_triangle_mesh.h>
 #include <igl/write_triangle_mesh.h>
+#include "CLI11.hpp"
 
 int main(int argc, char* argv[])
 {
+    CLI::App app{"Center a triangle mesh at the origin (0,0,0)."};
+
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
+    std::string in_file, out_file = "center.obj";
 
-    igl::read_triangle_mesh("/home/nico/git/libigl/tutorial/data/cow.off", V,F);
+    app.add_option("-i,--input", in_file, "The triangle mesh you want to check.")->required();
+    app.add_option("-o,--output", out_file, "The output files. Default is 'center.obj'.");
+
+    CLI11_PARSE(app, argc, argv);
+
+    igl::read_triangle_mesh(in_file, V,F);
 
     V = V.rowwise() - V.colwise().mean();
-    igl::write_triangle_mesh("center.obj", V, F);
+    igl::write_triangle_mesh(out_file, V, F);
     return 0;
 }
