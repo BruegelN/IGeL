b5d4fb8676629ddee5189bfc0b7f0a39852fc638
BruegelN <BruegelN@users.noreply.github.com>
2020-09-08 13:56:23 +0200

    Check if .obj has texture coordinates if so display radio button to toggle between 3d mesh and 2d texture. Use c++17 std::filesystems for file extension.

--- src/view.cpp
+++ src/view.cpp
@@ -1,18 +1,21 @@
 #include <igl/read_triangle_mesh.h>
+#include <igl/readOBJ.h>
 #include <igl/opengl/glfw/Viewer.h>
 #include <igl/opengl/glfw/imgui/ImGuiMenu.h>
 #include <igl/opengl/glfw/imgui/ImGuiHelpers.h>
 #include <imgui/imgui.h>
 #include <igl/png/writePNG.h>
+#include <filesystem>
 #include "CLI11.hpp"
 
+namespace fs = std::filesystem;
 
 int main(int argc, char *argv[])
 {
   CLI::App app{"Use the libigl viewer to view a triangle mesh."};
 
-  Eigen::MatrixXd V;
-  Eigen::MatrixXi F;
+  Eigen::MatrixXd V,TC;
+  Eigen::MatrixXi F,FTC;
   std::string in_file, screenshot_file = "screenshot.png";
 
   app.add_option("-i,--input", in_file, "The triangle mesh you want to view.")->required();
@@ -24,6 +27,14 @@ int main(int argc, char *argv[])
   {
     return 1;
   }
+
+  if(".obj" == fs::path(in_file).extension())
+  {
+    Eigen::MatrixXd N,FN;
+    igl::readOBJ(in_file,V,TC,N,F,FTC,FN);
+  }
+
+  const bool has_texture = [&]() -> bool { return (FTC.rows() == F.rows() && TC.rows() >= V.rows());}();
 
   // Init the viewer
   igl::opengl::glfw::Viewer viewer;
@@ -54,6 +65,21 @@ int main(int argc, char *argv[])
       // Save it to a PNG
       igl::png::writePNG(R,G,B,A,std::to_string(num_screenshot)+screenshot_file);
       num_screenshot++;
+    }
+    ImGui::Separator();
+    if(has_texture)
+    {
+      static int e = 0;
+      if (ImGui::RadioButton("3d mesh",&e,0))
+      {
+        viewer.data().clear();
+        viewer.data().set_mesh(V, F);
+      }
+      if (ImGui::RadioButton("2d texture",&e,1))
+      {
+        viewer.data().clear();
+        viewer.data().set_mesh(TC, FTC);
+      }
     }
   };
 
