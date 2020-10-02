aa94a10a65d14650a9a209a369da7385ec2ae564
Nico Brügel <bruegeln@crashing.systems>
2020-10-02 10:46:01 +0200

    simplify split, through the magic of igl::slice_mask()

--- src/split.cpp
+++ src/split.cpp
@@ -23,28 +23,16 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    igl::facet_components(F,C);
-    std::vector<Eigen::MatrixXi> vec;
-    for(int i = 0; i <= C.maxCoeff(); i++)
+    const size_t n = igl::facet_components(F,C);
+    for(size_t i = 0; i < n; i++)
     {
-        vec.emplace_back(1,3);
-    }
-
-    for(int rowIndex = 0; rowIndex < F.rows(); rowIndex++)
-    {
-        int comp = C(rowIndex);
-        auto face = F.row(rowIndex);
-
-        vec.at(comp).conservativeResize(vec.at(comp).rows()+1, vec.at(comp).cols());
-        vec.at(comp).row(vec.at(comp).rows()-1) = face;
-    }
-
-    for(int i = 0; i <= C.maxCoeff(); i++)
-    {
-        Eigen::MatrixXi f = vec.at(i).bottomRows(vec.at(i).rows()-1);
-        Eigen::MatrixXd v = V;
+        Eigen::MatrixXd v;
+        Eigen::MatrixXi Fcomponent,f;
         Eigen::VectorXi I;
-        igl::remove_unreferenced(Eigen::MatrixXd(v),Eigen::MatrixXi(f),v,f,I);
+        // only keep faces of current component
+        igl::slice_mask(F,(C.array() == i),1,Fcomponent);
+        // get rid all the other vertices
+        igl::remove_unreferenced(Eigen::MatrixXd(V),Eigen::MatrixXi(Fcomponent),v,f,I);
         igl::write_triangle_mesh(out_file+std::to_string(i)+".obj", v, f);
     }
 }
